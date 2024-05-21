f69c8185919e3512222cac531e1dc7b731edcda2
MarceloSilveira1709 <marcelosilverag@hotmail.com>
2024-05-21 14:54:28 -0300

    subindo funcionalidades da calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,58 @@
 <script setup>
+import { reactive } from 'vue';
 
-  </script>
+import Formulario from './components/formulario.vue';
+
+const estado = reactive({
+  numero1: 0,
+  numero2: 0,
+  resultado: 0,
+  operador: 'somar',
+})
+
+function getNumber1(evento) {
+  estado.numero1 = evento.target.value;
+  if (estado.numero1 == '')  {
+    estado.numero1 = 0;
+  }
+  estado.numero1 = parseFloat(estado.numero1)
+  getOperacao();
+}
+
+function getNumber2(evento) {
+  estado.numero2 = evento.target.value;
+  if (estado.numero2 == '')  {
+    estado.numero2 = 0;
+  }
+  estado.numero2 = parseFloat(estado.numero2)
+  getOperacao();
+}
+
+function getOperador(evento) {
+  estado.operador = evento.target.value;
+  getOperacao();
+}
+
+const getOperacao = () => {
+  switch(estado.operador) {
+    case 'subtrair':
+      return estado.resultado = (estado.numero1 - estado.numero2);
+    case 'somar':
+      return estado.resultado = (estado.numero1 + estado.numero2);
+    case 'dividir':      
+      return estado.resultado = (estado.numero1 / estado.numero2);
+    case 'multiplicar':
+      return estado.resultado = (estado.numero1 * estado.numero2);
+  }
+}
+</script>
 
 <template>
-  <h1>ola</h1>
+  <div class="container">
+    <Cabecalho />
+    <Formulario :get-number-1="getNumber1" :get-number-2="getNumber2" :resultado="estado.resultado" :get-operador="getOperador"/>
+    <Rodape />
+  </div>
   
 </template>
 
---
+++ src/components/formulario.vue
@@ -0,0 +1,39 @@
+<script setup>
+    const props = defineProps(["getNumber1", "getNumber2", "getOperador", "resultado"]);
+</script>
+
+<template>
+    <form class="text-center">
+    <div class="row">
+        <div class="col">
+        <input required type="number" placeholder="0" class="form-control mb-4" @keyup="props.getNumber1"/> 
+        </div>
+        <div class="col-md-2 mb-4 ">
+        <select class="form-control " @change="props.getOperador">
+            <option class="subtrair " value="subtrair">-</option>
+            <option class="somar " selected value="somar">+</option>
+            <option class="dividir " value="dividir">÷</option>
+            <option class="multiplicar " value="multiplicar">x</option>
+        </select>
+        </div>
+        <div class="col">
+        <input required type="number" placeholder="0" class="form-control mb-4" @keyup="props.getNumber2"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+        <h3 >Resultado</h3>
+        </div>
+    </div>
+    <div class="row">
+    <div class="col">
+        <h2 >{{ props.resultado }}</h2>
+        </div>
+    </div>
+    </form>
+</template>
+
+<style scoped>
+
+
+</style>
